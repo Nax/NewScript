54dd28cee6d97c3bcbf3285fed7f6a0c0bb6ef4b
Nax <max@bacoux.com>
2018-11-06 02:31:50 +0100

    Skeleton

---
+++ include/newscript/newscript.h
@@ -0,0 +1,12 @@
+#ifndef NEWSCRIPT_H
+#define NEWSCRIPT_H
+
+#if defined(__cplusplus)
+# define NEWSCRIPT_API  extern "C"
+#else
+# define NEWSCRIPT_API
+#endif
+
+
+
+#endif
---
+++ src/newscript/main.c
@@ -0,0 +1,4 @@
+int main(int ragc, char** argv)
+{
+    return 0;
+}
