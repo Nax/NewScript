d0fd22f0e2e115397ac1b30db27d1c2c8dd63efa
Nax <max@bacoux.com>
2018-11-29 18:07:53 +0100

    WIP

--- src/libnewscript/thread.c
+++ src/libnewscript/thread.c
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include <libnewscript/thread.h>
 
+typedef struct {
+    uint32_t            cursor;
+    const NsBytecode*   bc;
+} NsBytecodeReader;
+
 extern uint64_t kNsThreadTable[];
 
 static void writeThreadData(NsThreadBuilder* builder, const void* data, size_t length)
@@ -40,17 +45,64 @@ static void writeThread64(NsThreadBuilder* builder, uint64_t value)
     writeThreadData(builder, &value, 8);
 }
 
+static void readBytecodeData(void* dst, NsBytecodeReader* reader, size_t length)
+{
+    memcpy(dst, reader->bc->code + reader->cursor, length);
+    reader->cursor += length;
+}
+
+static uint8_t readBytecode8(NsBytecodeReader* reader)
+{
+    uint8_t tmp;
+    readBytecodeData(&tmp, reader, 1);
+    return tmp;
+}
+
+static uint16_t readBytecode16(NsBytecodeReader* reader)
+{
+    uint16_t tmp;
+    readBytecodeData(&tmp, reader, 2);
+    return tmp;
+}
+
+static uint32_t readBytecode32(NsBytecodeReader* reader)
+{
+    uint32_t tmp;
+    readBytecodeData(&tmp, reader, 4);
+    return tmp;
+}
+
+static uint64_t readBytecode64(NsBytecodeReader* reader)
+{
+    uint64_t tmp;
+    readBytecodeData(&tmp, reader, 8);
+    return tmp;
+}
+
+static uint16_t readBytecodeReg(NsBytecodeReader* reader)
+{
+    uint16_t reg = readBytecode8(reader);
+    if (reg & 0x80)
+    {
+        reg &= 0x7f;
+        reg |= (readBytecode8(reader) << 7);
+    }
+    return reg;
+}
+
 void* nsThreadBuild(NsVirtualMachine* vm, NsBytecode* bc)
 {
-    NsThreadBuilder builder;
-    size_t i;
+    NsBytecodeReader    reader;
+    NsThreadBuilder     builder;
     uint8_t op;
+
+    reader.bc = bc;
+    reader.cursor = 0;
 
     builder.size = 0;
     builder.capacity = 128;
     builder.data = malloc(builder.capacity);
 
-    i = 0;
     for (;;)
     {
         if (i >= bc->codeSize)
